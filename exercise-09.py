f0b2f4477b4128b5c560a1c82a3580aa48a2b7b8
Jibundeyare <contact@jibundeyare.com>
2020-11-02 15:19:21 +0100

    Ajout d'exercices

---
+++ exercise-09.py
@@ -0,0 +1,114 @@
+# exercise-09-solution.py
+
+import random
+
+# exo 6.1
+# Créez un dictionnaire nommé `my_dict` associant :
+# - une clé alphanumérique et un nombre entier
+# - une clé alphanumérique et un nombre à virgule flottante
+# - une clé alphanumérique et une chaîne de caractères
+# - une clé alphanumérique et un booléen
+# Puis affichez le résultat avec un simple `print()`
+
+# réponse 6.1
+
+# exo 6.2
+# Créez un dictionnaire nommé `my_dict` associant :
+# - une clé booléenne et un nombre entier
+# - une clé booléenne et un nombre à virgule flottante
+# - une clé nuémrique et une chaîne de caractères
+# - une clé alphanumérique et un booléen
+# Puis affichez le résultat avec un simple `print()`
+
+# réponse 6.2
+
+# exo 6.3
+# Ajoutez au dictionnaire un élément qui associe la clé alphanumérique `ipsum` à la valeur `2.71`
+# Puis affichez le résultat avec un simple `print()`
+my_dict = {
+    'foo': 42,
+    'bar': 3.14,
+    'baz': 'lorem ipsum',
+    'lorem': True
+}
+
+# réponse 6.3
+
+# exo 6.4
+# Supprimez du dictionnaire la clé `foo`
+# Puis affichez le résultat avec un simple `print()`
+my_dict = {
+    'foo': 42,
+    'bar': 3.14,
+    'baz': 'lorem ipsum',
+    'lorem': True
+}
+
+# réponse 6.4
+
+# exo 6.5
+# Remplacez la valeur du dictionnaire associée à la clé `foo` par `123`
+# Puis affichez le résultat avec un simple `print()`
+my_dict = {
+    'foo': 42,
+    'bar': 3.14,
+    'baz': 'lorem ipsum',
+    'lorem': True
+}
+
+# réponse 6.5
+
+# exo 6.6
+# En utilisant une boucle `for`, affichez les clés (et pas les valeurs) qui se trouvent dans le dictionnaire
+my_dict = {
+    'foo': 42,
+    'bar': 3.14,
+    'baz': 'lorem ipsum',
+    'lorem': True
+}
+
+# réponse 6.6
+
+# exo 6.7
+# En utilisant une boucle `for`, affichez les valeurs (et pas les clés) qui se trouvent dans le dictionnaire
+my_dict = {
+    'foo': 42,
+    'bar': 3.14,
+    'baz': 'lorem ipsum',
+    'lorem': True
+}
+
+# réponse 6.7
+
+# exo 6.8
+# En utilisant une boucle `for` et sans utiliser la méthode `items()`, affichez les clés et les valeurs qui se trouvent dans le dictionnaire
+my_dict = {
+    'foo': 42,
+    'bar': 3.14,
+    'baz': 'lorem ipsum',
+    'lorem': True
+}
+# Exemple de résultat attendu :
+# key: foo, value: 42
+# key: bar, value: 3.14
+# key: baz, value: lorem ipsum
+# etc...
+
+# réponse 6.8
+
+# exo 6.9
+# En utilisant une boucle `for` et la méthode `items()`, affichez les clés et les valeurs qui se trouvent dans le dictionnaire
+my_dict = {
+    'foo': 42,
+    'bar': 3.14,
+    'baz': 'lorem ipsum',
+    'lorem': True
+}
+# Exemple de résultat attendu :
+# key: foo, value: 42
+# key: bar, value: 3.14
+# key: baz, value: lorem ipsum
+# etc...
+
+# réponse 6.9
+
