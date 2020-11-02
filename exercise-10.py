3275f266b81d98b4041755aa77fef753009e4907
Jibundeyare <contact@jibundeyare.com>
2020-11-02 16:18:07 +0100

    Ajout d'exercices

---
+++ exercise-10.py
@@ -0,0 +1,83 @@
+# exercise-10.py
+
+# exo 10.1
+# Créer une fonction nommée `my_sum()` qui :
+# - prend deux paramètres
+# - additionne ces deux paramètres
+# - renvoit le résultat
+# Appelez la fonction et affichez le résultat
+
+# réponse 10.1
+
+# exo 10.2
+# Créer une fonction nommée `my_diff()` qui :
+# - prend deux paramètres de type `int`
+# - additionne ces deux paramètres
+# - renvoit le résultat de type `int`
+# Notez bien le type hinting dans la déclaration de la fonction
+# Appelez la fonction et affichez le résultat
+
+# réponse 10.2
+
+# exo 10.3
+# Créer une fonction nommée `oui_non()` qui :
+# - prend un paramètre booléen
+# - renvoit `oui` si le boolest est égal à True
+# - renvoit `non` si le boolest est égal à False
+
+# réponse 10.3
+
+# exo 10.4
+# Créer une fonction nommée `is_greater()` qui :
+# - prend deux paramètres `a` et `b` de type `float`
+# - renvoit True si `a` est plus grand que `b`
+# Appelez la fonction et affichez le résultat
+
+# réponse 10.4
+
+# exo 10.5
+# Créer une fonction nommée `compare()` qui :
+# - prend deux paramètres `a` et `b` de type `float`
+# - renvoit 1 si `a` est plus grand que `b`
+# - renvoit -1 si `a` est plus petit que `b`
+# - renvoit 0 si `a` et `b` sont égaux
+# Appelez la fonction et affichez le résultat
+
+# réponse 10.5
+
+# exo 10.6
+# La formule suivante permet de converit des mètres en miles :
+#
+#       miles = meters / 1609.344
+#
+# La formule suivante permet de faire le contraire :
+#
+#       meters = miles * 1609.344
+#
+# Créez une fonction nommée :
+#
+# - metersToMiles() permettant de convertir des mètres en miles
+# - milesToMeters() permettant de convertir des miles en mètres
+#
+# Ensuite convertissez les valeurs :
+#
+# - 100 mètres en miles
+# - 100 miles en mètres
+#
+# Appelez les fonctions et affichez les résultats
+
+# réponse 10.6
+
+# exo 10.7
+# Créer une fonction nommée `compute_tax()` qui :
+# - prend un paramètre nommé `price` de type `float`
+# - prend un paramètre nommé `tax_type` de type `int`
+# - ajoute une taxe de 2,1 % à `price` si `tax_type` ets égal à `1`
+# - ajoute une taxe de 5,5 % à `price` si `tax_type` ets égal à `2`
+# - ajoute une taxe de 10 % à `price` si `tax_type` ets égal à `3`
+# - ajoute une taxe de 20 % à `price` si `tax_type` ets égal à `4`
+# Référence : [Quels sont les taux de TVA en vigueur en France et dans l'Union européenne ? | economie.gouv.fr](https://www.economie.gouv.fr/cedef/taux-tva-france-et-union-europeenne)
+# Appelez la fonction et affichez le résultat avec un montant de 100 € pour chaque type de taxe
+
+# réponse 10.7
+
