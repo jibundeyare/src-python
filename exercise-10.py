a2114a4f5a557dc007111120c3ab26f9dbdc4675
Jibundeyare <contact@jibundeyare.com>
2020-11-04 22:31:30 +0100

    Correction

--- exercise-10.py
+++ exercise-10.py
@@ -74,10 +74,10 @@
 # Créer une fonction nommée `compute_tax()` qui :
 # - prend un paramètre nommé `price` de type `float`
 # - prend un paramètre nommé `tax_type` de type `int`
-# - ajoute une taxe de 2,1 % à `price` si `tax_type` ets égal à `1`
-# - ajoute une taxe de 5,5 % à `price` si `tax_type` ets égal à `2`
-# - ajoute une taxe de 10 % à `price` si `tax_type` ets égal à `3`
-# - ajoute une taxe de 20 % à `price` si `tax_type` ets égal à `4`
+# - ajoute une taxe de 2,1 % à `price` si `tax_type` est égal à `1`
+# - ajoute une taxe de 5,5 % à `price` si `tax_type` est égal à `2`
+# - ajoute une taxe de 10 % à `price` si `tax_type` est égal à `3`
+# - ajoute une taxe de 20 % à `price` si `tax_type` est égal à `4`
 # Référence : [Quels sont les taux de TVA en vigueur en France et dans l'Union européenne ? | economie.gouv.fr](https://www.economie.gouv.fr/cedef/taux-tva-france-et-union-europeenne)
 # Appelez la fonction et affichez le résultat avec un montant de 100 € pour chaque type de taxe
 
