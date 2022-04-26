5b2a58dcc423492c3c264c51e3ed5aaba214e5ed
jibundeyare <contact@jibundeyare.com>
2022-04-26 17:16:21 +0200

    Amélioration des énoncés des exos sur les variables

--- exercice-02-variables.py
+++ exercice-02-variables.py
@@ -2,11 +2,11 @@
 
 # exo 2.1
 # Affectez :
-# - le nombre 42 à une variable
-# - le nombre d'or 1,61 à une variable
-# - votre nom et prénom à une variable
-# - la valeur booléenne vrai si nous sommes le matin, sinon la valeur booléenne faux, à une variable
-# - la valeur nulle `None` à une variable
+# - le nombre 42 à une variable nommée `answer`
+# - le nombre d'or 1,61 à une variable nommée `golden_ratio`
+# - votre nom et prénom à une variable nommée `fullname`
+# - la valeur booléenne vraie si vous aimez Python ou fasse si ce n'est pas le cas, à une variable nommée `love_python`
+# - la valeur nulle `None` à une variable nommée `license_agreement`
 # Affichez ces variables
 
 # réponse 2.1
