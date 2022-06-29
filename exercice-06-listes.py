9f25f5226fe0d491c10aa1faab09b33e379af55c
jibundeyare <contact@jibundeyare.com>
2022-06-29 13:02:09 +0200

    Améliorations

--- exercice-06-listes.py
+++ exercice-06-listes.py
@@ -93,7 +93,7 @@ my_list = [2.71, 42, 123, 2, 3.14, 1.61]
 # réponse 6.13
 
 # exo 6.14
-# Créez une deuxième liste ne contenant que les nombre entiers de la liste
+# Créez une deuxième liste nommée `new_list` ne contenant que les nombre entiers de la liste
 my_list = [2.71, 42, 123, 2, 3.14, 1.61]
 
 # réponse 6.14
